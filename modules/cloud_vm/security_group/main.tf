b2d267907ee99aaaf0801621f94b67f7d2df9298
clgi-technology <ron.wright@clgi.org>
2025-06-11 12:52:01 -0500

    Update main.tf

--- modules/cloud_vm/security_group/main.tf
+++ modules/cloud_vm/security_group/main.tf
@@ -1,37 +1,37 @@
 resource "aws_security_group" "vm_sg" {
   name        = "${var.project_name}-vm-sg"
-  description = "Security group for VM allowing SSH, HTTP, and custom app ports"
+  description = "Security group for VM allowing SSH, HTTP, and HTTPS traffic"
   vpc_id      = var.vpc_id
-
-  ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ssh_cidrs
-  }
 
   ingress {
     description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = var.allowed_http_cidrs
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from any IP
   }
 
   ingress {
-    description = "Allow custom app port"
-    from_port   = var.app_port
-    to_port     = var.app_port
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = var.allowed_app_cidrs
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS from any IP
+  }
+
+  ingress {
+    description = "Allow SSH from specified IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_ip_address]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound traffic
   }
 
   tags = {
