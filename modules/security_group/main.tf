a1a26440bff24d542efb02990672971870dce7f6
clgi-technology <215250895+clgi-technology@users.noreply.github.com>
2025-06-11 15:38:07 -0500

    duplicate removed

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,28 +1,24 @@
-variable "ssh_ip_address" {
-  description = "CIDR block to allow SSH access"
-  type        = string
-}
+resource
+"aws_security_group" "ssh_sg" {
+    name        = "ssh-security-group"
+    description = "Allow SSH access from a given IP"
+    vpc_id      = var.vpc_id  # This assumes a VPC ID is passed in
 
-resource "aws_security_group" "ssh_sg" {
-  name        = "ssh-security-group"
-  description = "Allow SSH access from a given IP"
-  vpc_id      = var.vpc_id  # This assumes a VPC ID is passed in
+    ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = [var.ssh_ip_address]
+    }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.ssh_ip_address]
+    egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+      Name = "SSHAccessSG"
+    }
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "SSHAccessSG"
-  }
-}
