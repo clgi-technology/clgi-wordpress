801524a1f09c0c8a3ad1ff87581b51b10021254f
clgi-technology <ron.wright@clgi.org>
2025-06-11 18:45:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,9 +18,6 @@ terraform {
 # AWS Provider
 provider "aws" {
   region     = var.region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  token      = var.aws_session_token
 }
 
 # Google Cloud Provider
