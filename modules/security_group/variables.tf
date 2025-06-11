982dc2d0a6b9fd31698a90a31a46c06728dd5c61
clgi-technology <ron.wright@clgi.org>
2025-06-11 14:42:03 -0500

    Create variables.tf

---
+++ modules/security_group/variables.tf
@@ -0,0 +1,15 @@
+variable "ssh_ip_address" {
+  description = "CIDR block to allow SSH access (e.g. 203.0.113.0/32)"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC where the security group will be created"
+  type        = string
+}
+
+# Optional: Region if you're using it in your provider block
+# variable "aws_region" {
+#   description = "AWS region to deploy resources"
+#   type        = string
+# }
