3c2056858ab6740aa98fb383640bc66f33efecea
clgi-technology <ron.wright@clgi.org>
2025-06-11 14:29:59 -0500

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,1 +1,28 @@
-test
+variable "ssh_ip_address" {
+  description = "CIDR block to allow SSH access"
+  type        = string
+}
+
+resource "aws_security_group" "ssh_sg" {
+  name        = "ssh-security-group"
+  description = "Allow SSH access from a given IP"
+  vpc_id      = var.vpc_id  # This assumes a VPC ID is passed in
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_ip_address]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SSHAccessSG"
+  }
+}
