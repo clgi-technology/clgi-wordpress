ecd316db8b50576a7e6ba999af46ed79f5af358f
clgi-technology <ron.wright@clgi.org>
2025-06-11 17:11:25 -0500

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,24 +1,40 @@
-resource
-"aws_security_group" "ssh_sg" {
-    name        = "ssh-security-group"
-    description = "Allow SSH access from a given IP"
-    vpc_id      = var.vpc_id  # This assumes a VPC ID is passed in
+resource "aws_security_group" "web_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Allow HTTP, HTTPS, and SSH access"
+  vpc_id      = var.vpc_id
 
-    ingress {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = [var.ssh_ip_address]
-    }
-
-    egress {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    tags = {
-      Name = "SSHAccessSG"
-    }
+  ingress {
+    description = "Allow SSH from allowed IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_ip_address]
   }
+
+  ingress {
+    description = "Allow HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
