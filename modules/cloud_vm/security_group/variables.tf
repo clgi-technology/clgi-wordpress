38ac63f82ee02e0d5443cc90dd3ca6d02dd09610
clgi-technology <ron.wright@clgi.org>
2025-06-11 12:52:42 -0500

    Update variables.tf

--- modules/cloud_vm/security_group/variables.tf
+++ modules/cloud_vm/security_group/variables.tf
@@ -8,26 +8,7 @@ variable "vpc_id" {
   description = "VPC ID where the security group will be created"
 }
 
-variable "allowed_ssh_cidrs" {
-  type        = list(string)
-  description = "CIDR blocks allowed to SSH"
-  default     = ["0.0.0.0/0"]  # Change to more restrictive CIDRs in production!
-}
-
-variable "allowed_http_cidrs" {
-  type        = list(string)
-  description = "CIDR blocks allowed to HTTP"
-  default     = ["0.0.0.0/0"]
-}
-
-variable "allowed_app_cidrs" {
-  type        = list(string)
-  description = "CIDR blocks allowed to app port"
-  default     = ["0.0.0.0/0"]
-}
-
-variable "app_port" {
-  type        = number
-  description = "Custom app port to allow inbound traffic"
-  default     = 8000
+variable "ssh_ip_address" {
+  type        = string
+  description = "CIDR block for SSH access (e.g., your IP address)"
 }
