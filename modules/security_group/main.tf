06e0d1dd9139e3a8d259bae63ad1e5e067228fec
clgi-technology <ron.wright@clgi.org>
2025-06-11 21:32:43 -0500

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,3 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    google = {
+      source = "hashicorp/google"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "aws"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  session_token = var.aws_session_token
+  region  = var.region
+}
+
+provider "google" {
+  alias   = "gcp"
+  project = var.gcp_project
+  region  = var.region
+  credentials = file(var.gcp_key_file)
+}
+
+provider "azurerm" {
+  alias   = "azure"
+  client_id     = var.azure_client_id
+  client_secret = var.azure_secret
+  tenant_id     = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+}
+
 resource "aws_security_group" "web_sg" {
   name        = "${var.project_name}-sg"
   description = "Allow HTTP, HTTPS, and SSH access"
