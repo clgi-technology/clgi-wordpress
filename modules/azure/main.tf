fa5fb6f148b70b93e1dab645a83098152e02647b
clgi-technology <ron.wright@clgi.org>
2025-06-13 00:41:22 -0500

    Create main.tf

---
+++ modules/azure/main.tf
@@ -0,0 +1,70 @@
+provider "azurerm" {
+  features {}
+
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_secret
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.vm_name}"
+  location = var.region
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${var.vm_name}"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.region
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-${var.vm_name}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-${var.vm_name}"
+  location            = var.region
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm.id
+  }
+}
+
+resource "azurerm_public_ip" "vm" {
+  name                = "${var.vm_name}-ip"
+  location            = var.region
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = var.vm_name
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = var.region
+  size                  = var.vm_size
+  admin_username        = "adminuser"
+  admin_password        = "Password1234!"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    name                 = "${var.vm_name}-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
