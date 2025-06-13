58c39094d0b335f997612458de6ce3057da8f4ba
clgi-technology <ron.wright@clgi.org>
2025-06-13 00:41:49 -0500

    Create variables.tf

---
+++ modules/azure/variables.tf
@@ -0,0 +1,7 @@
+variable "azure_client_id" {}
+variable "azure_secret" {}
+variable "azure_tenant_id" {}
+variable "azure_subscription_id" {}
+variable "region" {}
+variable "vm_name" {}
+variable "vm_size" {}
