9556a80c3987c9089dfdb4e4a66c0598215edec3
clgi-technology <ron.wright@clgi.org>
2025-06-12 19:36:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,12 +43,11 @@ provider "azurerm" {
   subscription_id = var.cloud_provider == "Azure" ? var.azure_subscription_id : null
 }
 
-# AWS VPC (only create if vpc_id is not provided)
+# AWS VPC
 resource "aws_vpc" "terraform" {
-  count                  = var.vpc_id == "" ? 1 : 0
-  cidr_block             = "10.0.0.0/16"
-  enable_dns_support     = true
-  enable_dns_hostnames   = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
     Name = "terraform-vpc"
@@ -57,9 +56,7 @@ resource "aws_vpc" "terraform" {
 
 # AWS Subnet
 resource "aws_subnet" "terraform" {
-  count = var.vpc_id == "" ? 1 : 0
-
-  vpc_id                  = aws_vpc.terraform[count.index].id
+  vpc_id                  = aws_vpc.terraform.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "${var.region}a"
   map_public_ip_on_launch = true
@@ -69,30 +66,11 @@ resource "aws_subnet" "terraform" {
   }
 }
 
-# TLS key generation
-resource "tls_private_key" "generated_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-data "tls_public_key" "generated_key" {
-  depends_on        = [tls_private_key.generated_key]
-  private_key_pem   = tls_private_key.generated_key.private_key_pem
-}
-
-resource "aws_key_pair" "key_pair" {
-  count      = var.use_existing_key_pair ? 0 : 1
-  key_name   = "generated-key"
-  public_key = data.tls_public_key.generated_key.public_key_openssh
-}
-
-# AWS Security Group
+# Security Group
 resource "aws_security_group" "terraform" {
-  count  = var.vpc_id == "" ? 1 : 0
-  vpc_id = var.vpc_id != "" ? var.vpc_id : aws_vpc.terraform[count.index].id
-
   name        = "terraform-sg"
-  description = "Default security group"
+  description = "Allow all TCP from allowed IP"
+  vpc_id      = aws_vpc.terraform.id
 
   ingress {
     from_port   = 0
@@ -109,15 +87,50 @@ resource "aws_security_group" "terraform" {
   }
 }
 
+# Generate SSH key
+resource "tls_private_key" "generated_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+data "tls_public_key" "generated_key" {
+  private_key_pem = tls_private_key.generated_key.private_key_pem
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "generated-key"
+  public_key = data.tls_public_key.generated_key.public_key_openssh
+}
+
+# Save private key locally
+resource "local_file" "private_key" {
+  filename        = "${path.module}/generated-key.pem"
+  content         = tls_private_key.generated_key.private_key_pem
+  file_permission = "0600"
+}
+
+# Ubuntu AMI Lookup
+data "aws_ami" "latest_ubuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  provider = aws.aws
+}
+
 # EC2 Instance
 resource "aws_instance" "vm" {
   count         = var.cloud_provider == "AWS" ? 1 : 0
   ami           = data.aws_ami.latest_ubuntu.id
   instance_type = var.vm_size
-  key_name      = var.use_existing_key_pair ? var.existing_key_pair_name : aws_key_pair.key_pair[0].key_name
+  key_name      = aws_key_pair.key_pair.key_name
 
-  subnet_id = var.vpc_id == "" ? aws_subnet.terraform[0].id : var.subnet_id
-  vpc_security_group_ids = var.vpc_id == "" ? [aws_security_group.terraform[0].id] : [var.security_group_id]
+  subnet_id              = aws_subnet.terraform.id
+  vpc_security_group_ids = [aws_security_group.terraform.id]
 
   user_data = templatefile("${path.module}/user_data.sh.tmpl", {
     deployment_mode   = var.deployment_mode,
@@ -133,31 +146,8 @@ resource "aws_instance" "vm" {
   provider = aws.aws
 }
 
-# AWS AMI lookup
-data "aws_ami" "latest_ubuntu" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  provider = aws.aws
-}
-
-# OUTPUT: Public IP
+# Output
 output "vm_ip" {
-  value = (
-    var.cloud_provider == "AWS" ? aws_instance.vm[0].public_ip : null
-  )
+  value       = aws_instance.vm[0].public_ip
   description = "Public IP of the deployed instance"
-}
-
-# OUTPUT: Private key file
-resource "local_file" "private_key" {
-  count           = var.use_existing_key_pair ? 0 : 1
-  filename        = "${path.module}/generated-key.pem"
-  content         = tls_private_key.generated_key.private_key_pem
-  file_permission = "0600"
 }
