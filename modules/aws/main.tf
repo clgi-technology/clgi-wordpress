82150fef3e3d50ee242a8f696e90e97649b3e025
clgi-technology <ron.wright@clgi.org>
2025-06-13 00:38:41 -0500

    Create main.tf

---
+++ modules/aws/main.tf
@@ -0,0 +1,92 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "${var.vm_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.region}a"
+
+  tags = {
+    Name = "${var.vm_name}-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vm_name}-igw"
+  }
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vm_name}-rt"
+  }
+}
+
+resource "aws_route" "default" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "ssh" {
+  name        = "${var.vm_name}-sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allowed_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated" {
+  key_name   = "${var.vm_name}-key"
+  public_key = tls_private_key.key.public_key_openssh
+}
+
+resource "aws_instance" "vm" {
+  ami                         = "ami-0c94855ba95c71c99" # Amazon Linux 2 (us-west-2)
+  instance_type               = var.vm_size
+  subnet_id                   = aws_subnet.public.id
+  key_name                    = aws_key_pair.generated.key_name
+  vpc_security_group_ids      = [aws_security_group.ssh.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = var.vm_name
+  }
+}
