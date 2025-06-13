5c17e447c6d306cce6250318090e518c20f6f732
clgi-technology <ron.wright@clgi.org>
2025-06-13 18:48:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,73 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# AWS
+provider "aws" {
+  region = var.region
+  alias  = "aws"
+}
+
+# GCP
+provider "google" {
+  credentials = file(var.gcp_key_file)
+  project     = var.gcp_project
+  region      = var.region
+  zone        = var.zone
+  alias       = "google"
+}
+
+# Azure
+provider "azurerm" {
+  features {}
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_secret
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+  alias           = "azurerm"
+}
+
+##############################################################################
+# AWS Module
+##############################################################################
+
 module "aws" {
-  source  = "./modules/aws"
-  count   = var.cloud_provider == "AWS" ? 1 : 0
+  source = "./modules/aws"
+  count  = var.cloud_provider == "AWS" ? 1 : 0
 
   providers = {
     aws = aws
   }
 
-  vm_name              = var.vm_name
-  vm_size              = var.vm_size
-  region               = var.region
-  ssh_ip_address       = var.ssh_ip_address
-  ssh_password         = var.ssh_password
-  setup_demo_clone     = var.setup_demo_clone
-  clone_target_url     = var.clone_target_url
-  deployment_mode      = var.deployment_mode
+  vm_name               = var.vm_name
+  vm_size               = var.vm_size
+  region                = var.region
+  ssh_ip_address        = var.ssh_ip_address
+  ssh_password          = var.ssh_password
+  setup_demo_clone      = var.setup_demo_clone
+  clone_target_url      = var.clone_target_url
+  deployment_mode       = var.deployment_mode
   auto_delete_after_24h = var.auto_delete_after_24h
 }
+
+##############################################################################
+# GCP Module
+##############################################################################
 
 module "gcp" {
   source = "./modules/gcp"
@@ -25,17 +77,20 @@ module "gcp" {
     google = google
   }
 
-  vm_name              = var.vm_name
-  vm_size              = var.vm_size
-  region               = var.region
-  zone                 = var.zone
-  ssh_ip_address       = var.ssh_ip_address
-  ssh_password         = var.ssh_password
-  setup_demo_clone     = var.setup_demo_clone
-  clone_target_url     = var.clone_target_url
-  deployment_mode      = var.deployment_mode
+  vm_name               = var.vm_name
+  vm_size               = var.vm_size
+  region                = var.region
+  zone                  = var.zone
+  ssh_public_key_path   = var.ssh_public_key_path
+  setup_demo_clone      = var.setup_demo_clone
+  clone_target_url      = var.clone_target_url
+  deployment_mode       = var.deployment_mode
   auto_delete_after_24h = var.auto_delete_after_24h
 }
+
+##############################################################################
+# Azure Module
+##############################################################################
 
 module "azure" {
   source = "./modules/azure"
@@ -45,13 +100,18 @@ module "azure" {
     azurerm = azurerm
   }
 
-  vm_name              = var.vm_name
-  vm_size              = var.vm_size
-  region               = var.region
-  ssh_ip_address       = var.ssh_ip_address
-  ssh_password         = var.ssh_password
-  setup_demo_clone     = var.setup_demo_clone
-  clone_target_url     = var.clone_target_url
-  deployment_mode      = var.deployment_mode
+  vm_name               = var.vm_name
+  vm_size               = var.vm_size
+  region                = var.region
+  ssh_ip_address        = var.ssh_ip_address
+  ssh_password          = var.ssh_password
+  setup_demo_clone      = var.setup_demo_clone
+  clone_target_url      = var.clone_target_url
+  deployment_mode       = var.deployment_mode
   auto_delete_after_24h = var.auto_delete_after_24h
+
+  azure_client_id       = var.azure_client_id
+  azure_secret          = var.azure_secret
+  azure_tenant_id       = var.azure_tenant_id
+  azure_subscription_id = var.azure_subscription_id
 }
