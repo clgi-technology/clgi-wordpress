06ad79f6518b5d86f86f2233a201dc13f92ed918
clgi-technology <ron.wright@clgi.org>
2025-06-13 00:36:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,66 +2,42 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
-    aws     = { source = "hashicorp/aws",     version = "~> 5.0" }
-    google  = { source = "hashicorp/google",  version = "~> 4.0" }
+    aws     = { source = "hashicorp/aws", version = "~> 5.0" }
+    google  = { source = "hashicorp/google", version = "~> 4.0" }
     azurerm = { source = "hashicorp/azurerm", version = "~> 3.0" }
   }
 }
 
-provider "aws" {
-  region = var.region
-  alias  = "aws"
-  count  = var.cloud_provider == "AWS" ? 1 : 0
-}
-
-provider "google" {
-  alias       = "google"
-  project     = var.gcp_project
-  region      = var.region
-  credentials = var.cloud_provider == "GCP" ? var.gcp_key_file : null
-  count       = var.cloud_provider == "GCP" ? 1 : 0
-}
-
-provider "azurerm" {
-  alias           = "azurerm"
-  features        = {}
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_secret
-  tenant_id       = var.azure_tenant_id
-  subscription_id = var.azure_subscription_id
-  count           = var.cloud_provider == "Azure" ? 1 : 0
-}
-
-# Conditional module loading
+# Conditional deployments
 module "aws" {
-  source          = "./modules/aws"
-  count           = var.cloud_provider == "AWS" ? 1 : 0
-  region          = var.region
-  vm_name         = var.vm_name
-  vm_size         = var.vm_size
-  ssh_cidr        = var.ssh_allowed_cidr
-  ssh_password    = var.ssh_password
-  deployment_mode = var.deployment_mode
-  clone_url       = var.clone_target_url
+  source         = "./modules/aws"
+  count          = var.cloud_provider == "AWS" ? 1 : 0
+  vm_name        = var.vm_name
+  region         = var.region
+  vm_size        = var.vm_size
+  ssh_allowed_ip = var.ssh_allowed_ip
 }
 
 module "gcp" {
-  source          = "./modules/gcp"
-  count           = var.cloud_provider == "GCP" ? 1 : 0
-  region          = var.region
-  zone            = var.zone
-  gcp_project     = var.gcp_project
-  vm_name         = var.vm_name
-  vm_size         = var.vm_size
-  ssh_pub_path    = var.ssh_public_key_path
-  deployment_mode = var.deployment_mode
+  source              = "./modules/gcp"
+  count               = var.cloud_provider == "GCP" ? 1 : 0
+  vm_name             = var.vm_name
+  region              = var.region
+  zone                = var.zone
+  vm_size             = var.vm_size
+  ssh_public_key_path = var.ssh_public_key_path
+  gcp_project         = var.gcp_project
+  gcp_credentials     = var.gcp_key_file
 }
 
 module "azure" {
-  source              = "./modules/azure"
-  count               = var.cloud_provider == "Azure" ? 1 : 0
-  region              = var.region
-  vm_name             = var.vm_name
-  vm_size             = var.vm_size
-  deployment_mode     = var.deployment_mode
+  source                = "./modules/azure"
+  count                 = var.cloud_provider == "Azure" ? 1 : 0
+  vm_name               = var.vm_name
+  region                = var.region
+  vm_size               = var.vm_size
+  azure_client_id       = var.azure_client_id
+  azure_secret          = var.azure_secret
+  azure_tenant_id       = var.azure_tenant_id
+  azure_subscription_id = var.azure_subscription_id
 }
