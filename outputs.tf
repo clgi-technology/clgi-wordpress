b1735264be6939c002acd5679804e94fddce3266
clgi-technology <ron.wright@clgi.org>
2025-06-13 14:19:48 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,14 @@
 output "aws_ip" {
-  value       = module.aws[0].vm_ip
+  value       = var.cloud_provider == "AWS" ? module.aws[0].vm_ip : null
   description = "AWS Instance Public IP"
-  condition   = var.cloud_provider == "AWS"
 }
 
 output "gcp_ip" {
-  value       = module.gcp[0].vm_ip
+  value       = var.cloud_provider == "GCP" ? module.gcp[0].vm_ip : null
   description = "GCP Instance Public IP"
-  condition   = var.cloud_provider == "GCP"
 }
 
 output "azure_ip" {
-  value       = module.azure[0].vm_ip
+  value       = var.cloud_provider == "Azure" ? module.azure[0].vm_ip : null
   description = "Azure VM Public IP"
-  condition   = var.cloud_provider == "Azure"
 }
