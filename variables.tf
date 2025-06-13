13afa08b7c10293f6c407601256a1d9986b15e68
clgi-technology <ron.wright@clgi.org>
2025-06-12 19:31:41 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "cloud_provider" {
   description = "The cloud provider to use (AWS, GCP, Azure)"
   type        = string
-  default     = "AWS"  # Default to AWS
+  default     = "AWS"
 }
 
 # Azure Variables
@@ -37,7 +37,7 @@ variable "gcp_key_file" {
   type        = string
 }
 
-# AWS Variables
+# AWS Credentials (optional if using IAM roles/environment vars)
 variable "aws_access_key" {
   description = "AWS Access Key"
   type        = string
@@ -49,27 +49,21 @@ variable "aws_secret_key" {
 }
 
 variable "aws_session_token" {
-  description = "AWS Session Token"
-  type        = string
-}
-
-# Project and Network Variables
-variable "project_name" {
-  description = "The name of the project"
-  type        = string
-  default     = "my-project"  # optional default or remove if you want to provide explicitly
-}
-
-variable "vpc_id" {
-  description = "The AWS VPC ID (leave blank to let Terraform create one)"
+  description = "AWS Session Token (optional)"
   type        = string
   default     = ""
 }
 
-# Networking and SSH
+# Networking and Project
+variable "region" {
+  description = "Cloud region (e.g., us-west-2, us-central1, eastus)"
+  type        = string
+}
+
 variable "ssh_ip_address" {
   description = "The SSH IP address (CIDR block)"
   type        = string
+  default     = "0.0.0.0/0"
 }
 
 variable "ssh_password" {
@@ -79,23 +73,13 @@ variable "ssh_password" {
 }
 
 # VM Configuration
-variable "region" {
-  description = "Cloud region (e.g., us-west-2, us-central1, eastus)"
+variable "vm_name" {
+  description = "Name of the VM"
   type        = string
 }
 
 variable "vm_size" {
   description = "VM size (AWS: t3.medium, GCP: e2-medium, Azure: Standard_B1s)"
-  type        = string
-}
-
-variable "security_group_id" {
-  type    = string
-  default = ""  # or set a default for known SG
-}
-
-variable "vm_name" {
-  description = "Name of the VM"
   type        = string
 }
 
@@ -118,7 +102,7 @@ variable "clone_target_url" {
   default     = ""
 }
 
-# Optional: Database / SMTP
+# Optional Secrets
 variable "db_password" {
   description = "Database password (used for WordPress or Django if applicable)"
   type        = string
@@ -132,58 +116,3 @@ variable "smtp_password" {
   sensitive   = true
   default     = ""
 }
-
-# SSH and Key Management
-variable "use_existing_key_pair" {
-  description = "Use an existing key pair instead of generating a new one"
-  type        = bool
-  default     = false
-}
-
-variable "existing_key_pair_name" {
-  description = "Name of the existing key pair (if applicable)"
-  type        = string
-  default     = ""
-}
-
-# VPC / Networking
-variable "use_existing_vpc" {
-  description = "Use an existing VPC"
-  type        = bool
-  default     = false
-}
-
-variable "existing_vpc_id" {
-  description = "Existing VPC ID to use (if applicable)"
-  type        = string
-  default     = ""
-}
-
-
-variable "cloud_provider" {}
-variable "region" {}
-variable "vm_size" {}
-variable "vm_name" {}
-variable "vpc_id" {
-  default = ""
-}
-variable "subnet_id" {
-  default = ""
-}
-variable "security_group_id" {
-  default = ""
-}
-variable "use_existing_key_pair" {
-  type    = bool
-  default = false
-}
-variable "existing_key_pair_name" {
-  default = ""
-}
-variable "ssh_ip_address" {
-  default = "0.0.0.0/0"
-}
-variable "deployment_mode" {}
-variable "setup_demo_clone" {}
-variable "ssh_password" {}
-variable "clone_target_url" {}
