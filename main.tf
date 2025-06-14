2235d2f6411b897fa54a734d1aa97927b1675c2d
clgi-technology <ron.wright@clgi.org>
2025-06-14 00:54:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,7 @@ module "aws" {
   source = "./modules/aws"
   count  = var.cloud_provider == "aws" ? 1 : 0
 
-  providers = {
-    aws = aws
-  }
-
+  
   region                = var.aws_region
   vm_name               = var.vm_name
   vm_size               = var.vm_size
@@ -24,10 +21,7 @@ module "gcp" {
   source = "./modules/gcp"
   count  = var.cloud_provider == "gcp" ? 1 : 0
 
-  providers = {
-    google = google
-  }
-
+  
   gcp_project           = var.gcp_project
   gcp_credentials       = var.gcp_credentials
   region                = var.gcp_region
@@ -47,9 +41,7 @@ module "azure" {
   source = "./modules/azure"
   count  = var.cloud_provider == "azure" ? 1 : 0
 
-  providers = {
-    azurerm = azurerm
-  }
+  
 
   azure_client_id       = var.azure_client_id
   azure_secret          = var.azure_secret
