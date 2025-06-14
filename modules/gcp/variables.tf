f1eeb7ee40988cd90e35f3067e3fc33a0ece4373
clgi-technology <ron.wright@clgi.org>
2025-06-14 00:07:23 -0500

    Update variables.tf

--- modules/gcp/variables.tf
+++ modules/gcp/variables.tf
@@ -1,7 +1,23 @@
-variable "gcp_credentials" {}
 variable "gcp_project" {}
-variable "region" {}
-variable "zone" {}
+variable "gcp_credentials" {}
 variable "vm_name" {}
 variable "vm_size" {}
-variable "ssh_public_key_path" {}
+variable "region" {}
+variable "zone" {}
+variable "ssh_allowed_ip" {}
+
+variable "setup_demo_clone" {
+  default = false
+}
+
+variable "clone_target_url" {
+  default = null
+}
+
+variable "deployment_mode" {
+  default = "sandbox"
+}
+
+variable "auto_delete_after_24h" {
+  default = false
+}
