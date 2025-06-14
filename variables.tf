65a0cca174617f5af6549dd944122e61730a0638
clgi-technology <ron.wright@clgi.org>
2025-06-14 00:32:03 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,86 +1,145 @@
+# Root variables.tf
+
 variable "cloud_provider" {
-  description = "Which cloud provider to deploy: aws, gcp, or azure"
+  description = "Cloud provider to use: aws, gcp, or azure"
   type        = string
   default     = "aws"
 }
 
-variable "vm_name" {
-  description = "Name of the virtual machine"
+variable "deployment_mode" {
+  description = "Deployment mode, e.g. sandbox or production"
   type        = string
-  default     = "my-vm"
+  default     = "sandbox"
+}
+
+variable "setup_demo_clone" {
+  description = "Whether to setup demo clone"
+  type        = bool
+  default     = false
+}
+
+variable "auto_delete_after_24h" {
+  description = "Whether to auto delete resources after 24 hours"
+  type        = bool
+  default     = false
+}
+
+variable "vm_name" {
+  description = "VM instance name"
+  type        = string
 }
 
 variable "vm_size" {
-  description = "Size of the VM instance"
+  description = "VM instance size/type"
   type        = string
-  default     = "t2.micro"
 }
 
-variable "ssh_allowed_ip" {
-  description = "IP allowed to SSH to the VM"
-  type        = string
-  default     = "0.0.0.0/0"
-}
-
-# AWS specific variables
+# AWS variables
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
 }
 
-# GCP specific variables
+variable "ssh_allowed_ip" {
+  description = "IP CIDR allowed to SSH"
+  type        = string
+}
+
+variable "ssh_password" {
+  description = "SSH password"
+  type        = string
+  default     = null
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = null
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = null
+}
+
+variable "aws_session_token" {
+  description = "AWS session token (optional)"
+  type        = string
+  default     = null
+}
+
+# GCP variables
+variable "gcp_project" {
+  description = "GCP project ID"
+  type        = string
+  default     = null
+}
+
+variable "gcp_credentials" {
+  description = "Path to GCP credentials JSON"
+  type        = string
+  default     = null
+}
+
 variable "gcp_region" {
   description = "GCP region"
   type        = string
-  default     = "us-central1"
+  default     = null
 }
 
 variable "gcp_zone" {
   description = "GCP zone"
   type        = string
-  default     = "us-central1-a"
+  default     = null
 }
 
-variable "gcp_project" {
-  description = "GCP project ID"
-  type        = string
-  default     = ""
-}
-
-variable "gcp_credentials" {
-  description = "Path to GCP credentials JSON file"
-  type        = string
-  default     = ""
-}
-
-# Azure specific variables
-variable "azure_region" {
-  description = "Azure region"
-  type        = string
-  default     = "eastus"
-}
-
-variable "azure_subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-  default     = ""
-}
-
+# Azure variables
 variable "azure_client_id" {
-  description = "Azure client ID (service principal)"
+  description = "Azure Client ID"
   type        = string
-  default     = ""
+  default     = null
 }
 
-variable "azure_client_secret" {
-  description = "Azure client secret (service principal)"
+variable "azure_secret" {
+  description = "Azure Secret"
   type        = string
-  default     = ""
+  default     = null
 }
 
 variable "azure_tenant_id" {
-  description = "Azure tenant ID"
+  description = "Azure Tenant ID"
   type        = string
-  default     = ""
+  default     = null
+}
+
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+  default     = null
+}
+
+variable "azure_region" {
+  description = "Azure region"
+  type        = string
+  default     = null
+}
+
+# Optional secrets
+variable "clone_target_url" {
+  description = "Clone URL for demo"
+  type        = string
+  default     = null
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  default     = null
+}
+
+variable "smtp_password" {
+  description = "SMTP password"
+  type        = string
+  default     = null
 }
