d8266ef58254527e9be123430169226f0ef9dada
clgi-technology <ron.wright@clgi.org>
2025-06-14 11:54:22 -0500

    Update main.tf

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -1,12 +1,4 @@
 # module/aws/main.tf file
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
