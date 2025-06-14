413d8936289e4ad7e2324c965bc4823f910b4300
clgi-technology <ron.wright@clgi.org>
2025-06-13 23:43:32 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -31,3 +31,25 @@ variable "azure_tenant_id" {
 variable "azure_subscription_id" {
   default = ""
 }
+variable "vm_name" {}
+variable "vm_size" {}
+variable "region" {}
+
+variable "ssh_allowed_ip" {}
+
+# Optional: only if used in your module
+variable "ssh_password" {
+  default = null
+}
+variable "setup_demo_clone" {
+  default = false
+}
+variable "clone_target_url" {
+  default = null
+}
+variable "deployment_mode" {
+  default = "sandbox"
+}
+variable "auto_delete_after_24h" {
+  default = false
+}
