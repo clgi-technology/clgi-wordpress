d2a4337c1183da50935bcdb54c7ee887372cf813
clgi-technology <ron.wright@clgi.org>
2025-06-14 02:23:29 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,14 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
   }
 }
 
