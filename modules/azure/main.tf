aa9da47c677649e55ab0ab8df48f144047352414
clgi-technology <ron.wright@clgi.org>
2025-06-14 02:24:13 -0500

    Update main.tf

--- modules/azure/main.tf
+++ modules/azure/main.tf
@@ -1,11 +1,20 @@
-#provider "azurerm" {
-#  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
 
-#  subscription_id = var.azure_subscription_id
-#  client_id       = var.azure_client_id
-#  client_secret   = var.azure_secret
-#  tenant_id       = var.azure_tenant_id
-#}
+provider "azurerm" {
+  features  {}
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_secret
+  tenant_id       = var.azure_tenant_id
+}
+
 
 
 resource "azurerm_resource_group" "rg" {
