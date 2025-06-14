0bea84a55ee751da4ffb27839ec7891eef1b2e42
clgi-technology <ron.wright@clgi.org>
2025-06-14 00:37:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
 
 provider "aws" {
   region     = var.aws_region
