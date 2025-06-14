6d1e96567105d53db9c9fe0dc77af5af0a4fac4c
clgi-technology <ron.wright@clgi.org>
2025-06-14 11:47:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,22 @@
 # Root Main.tf file
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
 # AWS Module
 module "aws" {
   source    = "./modules/aws"
