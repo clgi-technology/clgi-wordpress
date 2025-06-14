60a6869ddff85394a6f29b4d0c61fa917fa374c6
clgi-technology <ron.wright@clgi.org>
2025-06-13 23:52:06 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,51 @@
-variable "vm_name" {}
-variable "vm_size" {}
-variable "region" {}
+# root/variables.tf
 
-variable "ssh_allowed_ip" {}
+variable "vm_name" {
+  description = "Name of the virtual machine"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "Size of the virtual machine"
+  type        = string
+}
+
+variable "region" {
+  description = "Region for deployment"
+  type        = string
+}
+
+variable "ssh_allowed_ip" {
+  description = "CIDR block for allowed SSH access"
+  type        = string
+}
 
 variable "ssh_password" {
-  default = null
+  description = "Optional SSH password (used in Azure)"
+  type        = string
+  default     = null
 }
 
 variable "setup_demo_clone" {
-  default = false
+  description = "Whether to clone a demo site"
+  type        = bool
+  default     = false
 }
 
 variable "clone_target_url" {
-  default = null
+  description = "URL of the site to clone"
+  type        = string
+  default     = null
 }
 
 variable "deployment_mode" {
-  default = "sandbox"
+  description = "Deployment mode: sandbox or production"
+  type        = string
+  default     = "sandbox"
 }
 
 variable "auto_delete_after_24h" {
-  default = false
+  description = "Auto-delete environment after 24 hours?"
+  type        = bool
+  default     = false
 }
