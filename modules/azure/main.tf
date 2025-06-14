21851fa48c4d46df6f6053976f96d7c6a5d31d04
clgi-technology <ron.wright@clgi.org>
2025-06-14 00:16:29 -0500

    Update main.tf

--- modules/azure/main.tf
+++ modules/azure/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.vm_name}"
   location = var.region
