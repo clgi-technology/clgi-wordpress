1b4ebc02cf58c65dfdcb825ce0f0afe09c1206f1
clgi-technology <ron.wright@clgi.org>
2025-06-14 01:06:29 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
-# Root provider file
-
 terraform {
   required_providers {
     aws = {
@@ -15,29 +13,27 @@ terraform {
       version = "~> 3.0"
     }
   }
-
-  required_version = ">= 1.3.0"
 }
 
 provider "aws" {
-  region     = var.region
+  region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   token      = var.aws_session_token
 }
 
 provider "google" {
-  credentials = file(var.gcp_key_file)
   project     = var.gcp_project
-  region      = var.region
-  zone        = var.zone
+  credentials = file(var.gcp_key_file)
+  region      = var.gcp_region
+  zone        = var.gcp_zone
 }
 
 provider "azurerm" {
-  features  {}
+  features {}
 
+  subscription_id = var.azure_subscription_id
   client_id       = var.azure_client_id
   client_secret   = var.azure_secret
   tenant_id       = var.azure_tenant_id
-  subscription_id = var.azure_subscription_id
 }
