75079f0245f54929ec4a71e91adb900268fb7268
clgi-technology <ron.wright@clgi.org>
2025-06-14 21:09:19 -0500

    Update outputs.tf

--- modules/azure/outputs.tf
+++ modules/azure/outputs.tf
@@ -1,3 +1,19 @@
 output "vm_ip" {
-  value = azurerm_public_ip.vm.ip_address
+  description = "Public IP of the Azure VM"
+  value       = azurerm_public_ip.public_ip.ip_address
+}
+
+output "instance_id" {
+  description = "Azure VM ID"
+  value       = azurerm_linux_virtual_machine.vm.id
+}
+
+output "tags" {
+  description = "Tags assigned to the Azure VM"
+  value       = azurerm_linux_virtual_machine.vm.tags
+}
+
+output "destroy_after" {
+  description = "Auto-delete tag value, if defined"
+  value       = try(azurerm_linux_virtual_machine.vm.tags["destroy_after"], null)
 }
