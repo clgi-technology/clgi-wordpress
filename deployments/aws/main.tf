aef72168f5d4b2c969d93289610f0097f078fb0b
clgi-technology <ron.wright@clgi.org>
2025-06-15 00:39:21 -0500

    Update main.tf

--- deployments/aws/main.tf
+++ deployments/aws/main.tf
@@ -10,11 +10,9 @@ terraform {
 }
 
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  token      = var.aws_session_token
+  region = var.aws_region
 }
+
 
 data "template_file" "user_data" {
   template = file("${path.module}/../../templates/user_data.sh.tmpl")
