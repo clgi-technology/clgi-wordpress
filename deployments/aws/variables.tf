589cbf0be21adb06b5d6a0419057fa7c4261398c
clgi-technology <ron.wright@clgi.org>
2025-06-14 20:56:16 -0500

    Update variables.tf

--- deployments/aws/variables.tf
+++ deployments/aws/variables.tf
@@ -1,75 +1,137 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-}
+#############################
+# 🌐 Common Variables
+#############################
 
-variable "aws_access_key" {
-  description = "AWS access key"
+variable "cloud_provider" {
   type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-}
-
-variable "aws_session_token" {
-  description = "AWS session token"
-  type        = string
-  default     = null
-}
-
-variable "vm_name" {
-  description = "Name of the VM"
-  type        = string
-}
-
-variable "vm_size" {
-  description = "Instance type"
-  type        = string
-}
-
-variable "ssh_allowed_ip" {
-  description = "CIDR block allowed to SSH"
-  type        = string
-}
-
-variable "ssh_public_key" {
-  description = "Public SSH key contents"
-  type        = string
-}
-
-variable "ssh_public_key_path" {
-  description = "Path to SSH public key file"
-  type        = string
-  default     = null
-}
-
-variable "ssh_password" {
-  description = "Password used for SSH (if applicable)"
-  type        = string
-  default     = null
+  description = "Cloud provider to deploy to (aws, gcp, azure)"
 }
 
 variable "deployment_mode" {
-  description = "App to deploy: 'sandbox' or 'production'"
   type        = string
-}
-
-variable "setup_demo_clone" {
-  description = "Whether to clone a demo site"
-  type        = bool
-  default     = false
-}
-
-variable "clone_target_url" {
-  description = "URL to clone site from"
-  type        = string
-  default     = ""
+  description = "Deployment mode: sandbox = Django, production = WordPress"
 }
 
 variable "auto_delete_after_24h" {
-  description = "Whether to automatically delete VM after 24h"
   type        = bool
+  description = "If true, mark resources for deletion after 24h"
   default     = false
+}
+
+variable "region" {
+  type        = string
+  description = "Region to deploy the VM in"
+}
+
+variable "vm_name" {
+  type        = string
+  description = "Name of the VM to deploy"
+}
+
+variable "vm_size" {
+  type        = string
+  description = "Size of the VM (machine type varies per cloud)"
+}
+
+variable "ssh_allowed_ip" {
+  type        = string
+  description = "CIDR range allowed to access VM via SSH"
+}
+
+#############################
+# 🔐 SSH & Access
+#############################
+
+variable "ssh_public_key" {
+  type        = string
+  description = "Public SSH key to install on the VM"
+  sensitive   = true
+}
+
+variable "ssh_private_key" {
+  type        = string
+  description = "Private SSH key used for provisioning"
+  sensitive   = true
+  default     = ""
+}
+
+variable "ssh_password" {
+  type        = string
+  description = "Optional password for SSH login"
+  sensitive   = true
+  default     = ""
+}
+
+#############################
+# ☁️ AWS-Specific Variables
+#############################
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key ID"
+  sensitive   = true
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Access Key"
+  sensitive   = true
+  default     = ""
+}
+
+variable "aws_session_token" {
+  type        = string
+  description = "AWS Session Token (for STS)"
+  sensitive   = true
+  default     = ""
+}
+
+#############################
+# ☁️ GCP-Specific Variables
+#############################
+
+variable "gcp_project_id" {
+  type        = string
+  description = "GCP Project ID"
+  default     = ""
+}
+
+variable "gcp_credentials" {
+  type        = string
+  description = "GCP Service Account JSON (optional, if not via env)"
+  sensitive   = true
+  default     = ""
+}
+
+#############################
+# ☁️ Azure-Specific Variables
+#############################
+
+variable "azure_client_id" {
+  type        = string
+  description = "Azure Client ID"
+  sensitive   = true
+  default     = ""
+}
+
+variable "azure_client_secret" {
+  type        = string
+  description = "Azure Client Secret"
+  sensitive   = true
+  default     = ""
+}
+
+variable "azure_subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+  sensitive   = true
+  default     = ""
+}
+
+variable "azure_tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+  sensitive   = true
+  default     = ""
 }
