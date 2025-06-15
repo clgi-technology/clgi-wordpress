fa16b20594cdf5aa23e983b63234850ceee8f4b6
clgi-technology <ron.wright@clgi.org>
2025-06-14 21:09:05 -0500

    Update outputs.tf

--- modules/gcp/outputs.tf
+++ modules/gcp/outputs.tf
@@ -1,3 +1,19 @@
 output "vm_ip" {
-  value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+  description = "External IP address of the GCP VM"
+  value       = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+}
+
+output "instance_id" {
+  description = "GCP VM name"
+  value       = google_compute_instance.vm.name
+}
+
+output "labels" {
+  description = "Labels assigned to the GCP instance"
+  value       = google_compute_instance.vm.labels
+}
+
+output "destroy_after" {
+  description = "Custom auto-delete label, if set"
+  value       = try(google_compute_instance.vm.labels["destroy_after"], null)
 }
