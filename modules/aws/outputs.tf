915ccb07053b55ad01fbec4df8bf56d5b021d565
clgi-technology <ron.wright@clgi.org>
2025-06-14 22:39:56 -0500

    Update outputs.tf

--- modules/aws/outputs.tf
+++ modules/aws/outputs.tf
@@ -17,3 +17,9 @@ output "destroy_after" {
   description = "Custom auto-delete or expiration tag, if set"
   value       = try(aws_instance.vm.tags["destroy_after"], null)
 }
+
+output "private_key_pem" {
+  description = "Private key to SSH into the instance"
+  value       = tls_private_key.key[0].private_key_pem
+  sensitive   = true
+}
