dd19b0e854a7775ede0dc92ab718705dfb2490be
clgi-technology <ron.wright@clgi.org>
2025-06-14 20:59:15 -0500

    Update main.tf

--- deployments/aws/main.tf
+++ deployments/aws/main.tf
@@ -1,36 +1,38 @@
+#deployemnts/aws/main.tf
+
 terraform {
+  required_version = ">= 1.4.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-
-  backend "local" {
-    path = "terraform.tfstate"
-  }
 }
 
 provider "aws" {
-  region     = var.aws_region
+  region     = var.region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   token      = var.aws_session_token
 }
 
-module "aws_vm" {
-  source = "../../modules/aws"
-
-  region                = var.aws_region
-  vm_name               = var.vm_name
-  vm_size               = var.vm_size
-  ssh_allowed_ip        = var.ssh_allowed_ip
-  ssh_public_key        = var.ssh_public_key
-  ssh_password          = var.ssh_password
-  deployment_mode       = var.deployment_mode
-  setup_demo_clone      = var.setup_demo_clone
-  clone_target_url      = var.clone_target_url
-  ssh_public_key_path   = var.ssh_public_key_path
-  auto_delete_after_24h = var.auto_delete_after_24h
+module "security_groups" {
+  source         = "../../modules/security_groups"
+  cloud_provider = "aws"
+  ssh_cidr       = var.ssh_allowed_ip
 }
 
+module "app" {
+  source               = "../../modules/aws"
+  vm_name              = var.vm_name
+  vm_size              = var.vm_size
+  region               = var.region
+  deployment_mode      = var.deployment_mode
+  ssh_public_key       = var.ssh_public_key
+  ssh_password         = var.ssh_password
+  auto_delete_after_24h = var.auto_delete_after_24h
+
+  security_group_id    = module.security_groups.id
+}
