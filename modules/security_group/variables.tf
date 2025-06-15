636b588dec23fa74a7c073a2039d4d8b0dd3289c
clgi-technology <ron.wright@clgi.org>
2025-06-14 23:00:49 -0500

    Update variables.tf

--- modules/security_group/variables.tf
+++ modules/security_group/variables.tf
@@ -12,3 +12,8 @@ variable "ssh_ip_address" {
   description = "The allowed IP address for SSH access"
   type        = string
 }
+
+variable "cloud_provider" {
+  description = "Cloud provider to use (AWS, GCP, or Azure)"
+  type        = string
+}
