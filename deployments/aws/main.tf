a6fc443dd4ddafa40dc450303b2efb9bac726b72
clgi-technology <ron.wright@clgi.org>
2025-06-14 22:10:53 -0500

    Update main.tf

--- deployments/aws/main.tf
+++ deployments/aws/main.tf
@@ -1,6 +1,5 @@
 terraform {
   required_version = ">= 1.4.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -16,10 +15,8 @@ provider "aws" {
   token      = var.aws_session_token
 }
 
-# Load cloud-init user data template
 data "template_file" "user_data" {
   template = file("${path.module}/../../templates/user-data.sh.tmpl")
-
   vars = {
     deployment_mode    = var.deployment_mode
     setup_demo_clone   = var.setup_demo_clone ? "true" : "false"
@@ -34,18 +31,19 @@ module "security_groups" {
 }
 
 module "app" {
-  source                = "../../modules/aws"
-  vm_name               = var.vm_name
-  vm_size               = var.vm_size
-  region                = var.region
-  deployment_mode       = var.deployment_mode
-  ssh_public_key        = var.ssh_public_key
-  ssh_password          = var.ssh_password
+  source               = "../../modules/aws"
+  vm_name              = var.vm_name
+  vm_size              = var.vm_size
+  region               = var.region
+  deployment_mode      = var.deployment_mode
+  ssh_public_key       = var.ssh_public_key
+  ssh_password         = var.ssh_password
   auto_delete_after_24h = var.auto_delete_after_24h
-  ssh_allowed_ip        = var.ssh_allowed_ip
-  setup_demo_clone      = var.setup_demo_clone
-  clone_target_url      = var.clone_target_url
-  user_data             = data.template_file.user_data.rendered
+  ssh_allowed_ip       = var.ssh_allowed_ip
+  setup_demo_clone     = var.setup_demo_clone
+  clone_target_url     = var.clone_target_url
+  ssh_public_key_path  = "" # Optional if unused in module
+  user_data            = data.template_file.user_data.rendered
 
-  security_group_id     = module.security_groups.id
+  security_group_id    = module.security_groups.id
 }
