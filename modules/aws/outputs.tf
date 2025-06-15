2f9dc0f82eeddb43f3ede2a884c10610c0b379f4
clgi-technology <ron.wright@clgi.org>
2025-06-14 21:08:48 -0500

    Update outputs.tf

--- modules/aws/outputs.tf
+++ modules/aws/outputs.tf
@@ -1,3 +1,19 @@
 output "vm_ip" {
-  value = aws_instance.vm.public_ip
+  description = "Public IP address of the AWS EC2 instance"
+  value       = aws_instance.vm.public_ip
+}
+
+output "instance_id" {
+  description = "AWS EC2 instance ID"
+  value       = aws_instance.vm.id
+}
+
+output "tags" {
+  description = "Tags assigned to the AWS instance"
+  value       = aws_instance.vm.tags
+}
+
+output "destroy_after" {
+  description = "Custom auto-delete or expiration tag, if set"
+  value       = try(aws_instance.vm.tags["destroy_after"], null)
 }
