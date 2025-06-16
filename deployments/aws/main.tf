160b4c8bf91698cd5b2b87e7d1ae6e373eed169b
clgi-technology <ron.wright@clgi.org>
2025-06-16 05:30:34 -0500

    Update main.tf

--- deployments/aws/main.tf
+++ deployments/aws/main.tf
@@ -1,19 +1,4 @@
-terraform {
-  required_version = ">= 1.4.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region != "" ? var.aws_region : var.region
-}
-
-
+# deployment/aws/main.tf
 data "template_file" "user_data" {
   template = file("${path.module}/templates/user_data.sh.tmpl")
 
