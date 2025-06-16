56a32acf6b8f5f82459bd22f9473320acc0dc7d6
clgi-technology <ron.wright@clgi.org>
2025-06-16 16:22:58 -0500

    Update main.tf

--- deployments/aws/main.tf
+++ deployments/aws/main.tf
@@ -1,4 +1,24 @@
-# deployment/aws/main.tf
+# Generate SSH Key Pair
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "${var.vm_name}-key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+output "ssh_private_key" {
+  value     = tls_private_key.ssh_key.private_key_pem
+  sensitive = true
+}
+
+output "ssh_public_key" {
+  value = tls_private_key.ssh_key.public_key_openssh
+}
+
+# User Data Script
 data "template_file" "user_data" {
   template = file("${path.module}/templates/user_data.sh.tmpl")
 
@@ -12,26 +32,28 @@ data "template_file" "user_data" {
 module "security_group" {
   source         = "./modules/security_group"
   project_name   = var.vm_name
-  vpc_id         = module.app.vpc_id    # Make sure module.app outputs vpc_id
+  vpc_id         = module.app.vpc_id
   ssh_ip_address = var.ssh_allowed_ip
   cloud_provider = "aws"
-
-  # Add any other variables your security_group module requires
 }
 
 module "app" {
   source                = "./modules/aws"
   vm_name               = var.vm_name
   vm_size               = var.vm_size
-  aws_region            = var.aws_region   # Pass aws_region, not region
+  aws_region            = var.aws_region
+  aws_access_key        = var.aws_access_key
+  aws_secret_key        = var.aws_secret_key
+  aws_session_token     = var.aws_session_token
   deployment_mode       = var.deployment_mode
   ssh_password          = var.ssh_password
-  auto_delete_after_24h = var.auto_delete_after_24h
+  ssh_private_key       = tls_private_key.ssh_key.private_key_pem
   ssh_allowed_ip        = var.ssh_allowed_ip
+  auto_delete_after_24h = var.auto_delete_after_24h
   setup_demo_clone      = var.setup_demo_clone
   clone_target_url      = var.clone_target_url
   user_data             = data.template_file.user_data.rendered
 
   security_group_id     = module.security_group.security_group_id
-  ssh_public_key        = ""  # key pair created inside module
+  ssh_public_key        = tls_private_key.ssh_key.public_key_openssh
 }
