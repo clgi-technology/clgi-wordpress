889ac7ffe284afba7acc395bc291b54b6cd47e5b
clgi-technology <ron.wright@clgi.org>
2025-06-16 02:25:14 -0500

    Update and rename deployments/backend.tf to deployments/aws/backend.tf

--- deployments/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "local" {
-    # relative path from deployments/aws to infra-state repo's terraform.tfstate file
-    path = "../../../infra-state/terraform/terraform.tfstate"
-  }
-}
