37fa4d5d0f7cc44d1011d770890fc2700d3f22dc
clgi-technology <ron.wright@clgi.org>
2025-06-16 05:34:39 -0500

    Update provider.tf

--- deployments/aws/provider.tf
+++ deployments/aws/provider.tf
@@ -10,14 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region != "" ? var.aws_region : var.region
-  
-  # Credentials will be loaded from environment variables by default:
-  # AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN
-}
-
-provider "aws" {
-  region     = var.aws_region
+  region     = var.aws_region != "" ? var.aws_region : var.region
   access_key = var.aws_access_key != "" ? var.aws_access_key : null
   secret_key = var.aws_secret_key != "" ? var.aws_secret_key : null
   token      = var.aws_session_token != "" ? var.aws_session_token : null
