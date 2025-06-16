9a37e276bc1f6497e7b127c63218b75a0bd1e9b3
clgi-technology <ron.wright@clgi.org>
2025-06-16 05:34:06 -0500

    Update provider.tf

--- deployments/aws/provider.tf
+++ deployments/aws/provider.tf
@@ -15,3 +15,10 @@ provider "aws" {
   # Credentials will be loaded from environment variables by default:
   # AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN
 }
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key != "" ? var.aws_access_key : null
+  secret_key = var.aws_secret_key != "" ? var.aws_secret_key : null
+  token      = var.aws_session_token != "" ? var.aws_session_token : null
+}
