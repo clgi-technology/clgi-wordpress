576ac5cc5c235540b8d3eacc37fbe4385a3b3004
clgi-technology <ron.wright@clgi.org>
2025-06-18 23:32:42 -0500

    Update outputs.tf

--- modules/aws/outputs.tf
+++ modules/aws/outputs.tf
@@ -1,29 +1,29 @@
 output "vpc_id" {
-  value = aws_vpc.main[0].id
+  value = try(aws_vpc.main[0].id, null)
 }
 
 output "instance_id" {
+  value = try(aws_instance.vm[0].id, null)
   description = "AWS EC2 instance ID"
-  value       = aws_instance.vm[0].id
 }
 
 output "tags" {
+  value = try(aws_instance.vm[0].tags, {})
   description = "Tags assigned to the AWS instance"
-  value       = aws_instance.vm[0].tags
 }
 
 output "destroy_after" {
+  value = try(aws_instance.vm[0].tags["destroy_after"], null)
   description = "Custom auto-delete or expiration tag, if set"
-  value       = try(aws_instance.vm[0].tags["destroy_after"], null)
 }
 
 output "private_key_pem" {
+  value     = try(tls_private_key.deployer_key[0].private_key_pem, null)
+  sensitive = true
   description = "Private key to SSH into the instance"
-  value       = tls_private_key.deployer_key[0].private_key_pem
-  sensitive   = true
 }
 
 output "public_ip" {
+  value = try(aws_instance.vm[0].public_ip, null)
   description = "Public IP address of the AWS EC2 instance"
-  value       = aws_instance.vm[0].public_ip
 }
