74dc2a2f398cb519612136441db750c777b4fad7
clgi-technology <ron.wright@clgi.org>
2025-06-24 00:58:53 -0500

    Delete modules/auto_delete directory

--- modules/auto_delete/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "lambda_function_name" {
-  description = "Lambda function that performs auto-delete"
-  value       = aws_lambda_function.auto_delete_lambda.function_name
-}
--- modules/auto_delete/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vm_name" {
-  description = "Name prefix for resources"
-  type        = string
-}
-
-variable "instance_id" {
-  description = "ID of the EC2 instance to auto-delete"
-  type        = string
-}
